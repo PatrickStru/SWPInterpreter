4a30af7737c07e45aeaae657890783e37459654d
Patrick Struger <pstruger@student.tugraz.at>
2018-05-08 12:01:18 +0200

    Refreshed Parser.scala.

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -5,13 +5,13 @@ class ExpParser extends JavaTokenParsers {
   // TODO Implement the expression parser and add additional parsers for terminal and non terminal symbols, where necessary.
   def program: Parser[Program] = ??? //{function, ";"}, expr
 
-  private val id : Parser[ID] = "/(a-z)(A-Z|a-z|0-9|?|_)*/".r ^^ {s => ID(s.toString)}
+  //private val id : Parser[ID] = "/(a-z)(A-Z|a-z|0-9|?|_)*/".r ^^ {s => ID(s.toString)}
 
   private val int : Parser[Number] = "[1-9][0-9]*|0|-[1-9][0-9]*".r ^^ {i => Number(i.toInt)}
 
   private val expr = int | boolean
 
-  private val variable = id
+  //private val variable = id
 
   private val boolean : Parser[Bool] = ("True"|"False") ^^ {bool => Bool(bool.toBoolean)}
 
