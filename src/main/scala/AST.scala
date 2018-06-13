62abab1acf8ed0a5f3acad7ccdbe2ca92849fcda
Patrick Struger <pstruger@student.tugraz.at>
2018-06-13 13:19:57 +0200

    Finished Parser?!. Implemented new testcase.

--- src/main/scala/AST.scala
+++ src/main/scala/AST.scala
@@ -1,7 +1,7 @@
 sealed trait Node
 
 case class Program(functions: List[FunctionDeclaration], main: Node) extends Node
-case class FunctionDeclaration(name: String, params: List[String], body: Node) extends Node
+case class FunctionDeclaration(name: String, params: List[Node], body: Node) extends Node
 
 // TODO Add case classes to represent the AST.
 case class ID(s: String) extends Node
--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -6,59 +6,44 @@ class ExpParser extends JavaTokenParsers {
 
   def program : Parser[Program] = prog
 
-  // working
-  def variable : Parser[Var] = id ^^ { case (v) => Var(v)}
+  val id : Parser[String] = "[a-z]([A-Z]|[a-z]|[0-9]|[?]|_)*".r
 
-  // working
-  val id : Parser[String] = "[a-z]([A-Z]|[a-z]|[0-9]|[?]|_)*".r // ^^ {s => ID(s.toString)}
-
-  // working
   val int : Parser[Number] = "[1-9][0-9]*|0|-[1-9][0-9]*".r ^^ {i => Number(i.toInt)}
 
-  // working
   val bool : Parser[Bool] = ("True"|"False") ^^ {bool => Bool(bool.toBoolean)}
 
-  // working
-  def cond : Parser[Cond] = "if" ~> expr ~ "then" ~ expr ~ "else" ~ expr ^^ {case (l ~_~ m ~_~ r) => Cond(l, m, r)}
+  def variable : Parser[Var] = id ^^ {case(v) => Var(v)}
 
-  // working
-  def braces : Parser[Braces] = "(" ~> expr <~ ")" ^^ {case (param) => Braces(param)}
-
-  // working
-  def var_ass : Parser[varAssign] = (id ~ "=" ~ expr <~ ";") ^^ {case (l ~_~ r) => varAssign(l, r)}
-
-  // working
-  def block : Parser[Block] = "{" ~> rep(expr <~ opt(";")) <~ "}" ^^ {case (l) => Block(l)}
-
-  // working
-  def var_dec : Parser[varDec1] = "$" ~> id ~ "=" ~ expr <~ ";" ^^ {case(l ~_~ r) => varDec1(l, r)}
-
-  // TODO should work
-  def record_dec : Parser[recordValueDec1] = "$" ~> id ~ "=" ~ expr ^^ {case(l ~_~ r) => recordValueDec1(l, r)}
-
-  // TODO should work
-  def record_def : Parser[recordDef] = "object" ~ "{" ~> rep(record_dec <~ ";") <~ "}" ^^ {case(l) => recordDef(l)}
-
-  // TODO should work
   def liste : Parser[list] = "[" ~> repsep(expr, ",") <~ "]" ^^ {case(l) => list(l)}
 
-  // TODO should work
-  def recordRef : Parser[Node] = call | block | record_def | braces | variable
+  def cond : Parser[Cond] = "if" ~> expr ~ "then" ~ expr ~ "else" ~ expr ^^ {case (l ~_~ m ~_~ r) => Cond(l, m, r)}
 
-  // TODO ???
+  def braces : Parser[Braces] = "(" ~> expr <~ ")" ^^ {case (param) => Braces(param)}
+
+  //def var_ass : Parser[varAssign] = (id ~ "=" ~ expr <~ opt(";")) ^^ {case (l ~_~ r) => varAssign(l, r)}
+  def var_ass : Parser[varAssign] = id ~ "=" ~ expr ^^ {case (l ~_~ r) => varAssign(l, r)}
+
+  //def var_dec : Parser[varDec1] = "$" ~> id ~ "=" ~ expr <~ ";" ^^ {case(l ~_~ r) => varDec1(l, r)}
+  def var_dec : Parser[varDec1] = "$" ~> id ~ "=" ~ expr ^^ {case(l ~_~ r) => varDec1(l, r)}
+
+  //def block : Parser[Block] = "{" ~> rep(expr <~ opt(";")) <~ "}" ^^ {case (l) => Block(l)}
+  def block : Parser[Block] = "{" ~> rep(expr <~ ";") <~ "}" ^^ {case (l) => Block(l)}
+
+  def record_dec : Parser[recordValueDec1] = "$" ~> id ~ "=" ~ expr ^^ {case(l ~_~ r) => recordValueDec1(l, r)}
+
+  def record_def : Parser[recordDef] = "object" ~ "{" ~> rep(record_dec <~ ";") <~ "}" ^^ {case(l) => recordDef(l)}
+
+  def recordRef : Parser[Node] = block | braces | call | record_def | variable
+
   def record_access : Parser[recordAccess] = recordRef ~ "." ~ id ^^ {case(l ~_~ r) => recordAccess(l, r)}
 
-  // TODO should work
-  def expr : Parser[Node] = call | block | var_dec | var_ass | cond | liste | record_def | int | bool | braces | record_access | variable
-
-  // TODO should work
   def call : Parser[Call] = id ~ "(" ~ repsep(expr, ",") <~ ")" ^^ {case(name ~_~ params) => Call(name.toString, params)}
 
-  // TODO should work
-  def function : Parser[FunctionDeclaration] = "fun" ~> id ~ "(" ~ repsep(id, ",") ~ ")" ~ "=" ~ expr ^^
+  def expr : Parser[Node] = block | record_access | call | var_ass | record_def | var_dec | cond | liste | int | bool | braces | variable
+
+  def function : Parser[FunctionDeclaration] = "fun" ~> id ~ "(" ~ repsep(variable, ",") ~ ")" ~ "=" ~ expr ^^
     {case (name ~_~ params ~_~_~ body) => FunctionDeclaration(name, params, body)}
 
-  // TODO should work
   def prog : Parser[Program] = rep(function <~ ";") ~ expr ^^ {case(f ~ e) => Program(f, e)}
 }
 
